49ada3c222d68afc0a19afc3766d1e46e6996938
Gabriel Bova <gabrielbova101@gmail.com>
2020-03-02 11:27:47 -0300

    Update files

--- src/com/company/ex4.java
+++ src/com/company/ex4.java
@@ -7,6 +7,7 @@ public class ex4 {
     public static void main(String[] args) {
         //Variáveis
         Scanner teclado = new Scanner(System.in);
+
         float numero;
 
         //Entradas
